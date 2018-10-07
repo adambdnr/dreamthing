0036210eaa8b5a23f82c06348d95c546e673c9e0
Adam Bodnar <adambodnar2@gmail.com>
2018-10-07 16:05:29 +0200

    init;

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,16 +79,16 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    ADS
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://laravel.com/docs">ASD</a>
+                    <a href="https://laracasts.com">ASD</a>
+                    <a href="https://laravel-news.com">ASD</a>
+                    <a href="https://nova.laravel.com">ASD</a>
+                    <a href="https://forge.laravel.com">ASD</a>
+                    <a href="https://github.com/laravel/laravel">AD</a>
                 </div>
             </div>
         </div>
